c45083dfc5c300a735cf9ff9ac1ee5c46913cd87
danielchowyc <daniellchow@gmail.com>
2020-07-14 01:07:13 +0100

    commented out unnecessary code

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const container = document.querySelector('#container');
 
 // Task 1
 const content = document.createElement('div');
-content.classList.add('content');
+// content.classList.add('content');
 content.textContent = 'This is the glorious text-content!';
 
 container.appendChild(content);
@@ -10,7 +10,7 @@ container.appendChild(content);
 // Task 2
 const p = document.createElement('p');
 p.textContent = "Hey I'm red!";
-p.classList.add('red');
+// p.classList.add('red');
 p.style.color = 'red';
 
 container.appendChild(p);
@@ -18,7 +18,7 @@ container.appendChild(p);
 
 // Task 3
 const h3 = document.createElement('h3');
-h3.classList.add('blue');
+// h3.classList.add('blue');
 h3.style.color = 'blue';
 h3.textContent = "I'm a blue h3!";
 container.appendChild(h3);
@@ -26,7 +26,7 @@ container.appendChild(h3);
 
 // Task 4
 const borderDiv = document.createElement('div');
-borderDiv.classList.add('border');
+// borderDiv.classList.add('border');
 borderDiv.style.background = 'pink';
 borderDiv.style.border = "thick solid black";
 
